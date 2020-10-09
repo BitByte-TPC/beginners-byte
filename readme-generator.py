2c0a07584915d35e4f2abe2d765b3dfc6c0e2d15
Priyansh Garg <priyanshgarg30@gmail.com>
2020-10-09 08:56:37 +0530

    Improve Readme

--- readme-generator.py
+++ readme-generator.py
@@ -18,12 +18,12 @@ repos = repo_json['repositories']
 repos = sorted(repos, key=lambda repo: repo['name'].lower())
 
 for repo in repos:
-    readme_output += f"- [{repo['name']}]({repo['link']})  \n  {repo['description']}  \n  \
+    readme_output += f"- [**{repo['name']}**]({repo['link']})  \n  > {repo['description']}  \n  \
         [![GitHub issues](https://img.shields.io/github/issues/{repo['link'][19:]}?color=pink&logo=github)]({repo['link']}/issues) ".replace('    ', '')
     for label in repo['labels']:
         shields_label = label.replace(' ', '%20')
         url_label = label.replace(' ', '+')
         readme_output += f"[![GitHub issues](https://img.shields.io/github/issues/{repo['link'][19:]}/{shields_label}?color=pink&logo=github)]({repo['link']}/issues?q=is%3Aissue+is%3Aopen+label%3A%22{url_label}%22) "
-    readme_output += '\n'
+    readme_output += '\n\n'
 
 readme_file.write(readme_output)
